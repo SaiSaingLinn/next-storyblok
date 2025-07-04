1c2ff4797240bff0c33ad5051b2a7fcbb7b21ee6
Sai Saing Linn <saisainglinn.me@gmail.com>
2025-07-04 09:42:58 +0630

    update: add bridgeOptions in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,23 @@ import { draftMode } from "next/headers";
 const fetchHomePage = async () => {
   // Enable draft mode for development
   const { isEnabled } = await draftMode();
-  const storyblokApi = getStoryblokApi()
+  const storyblokApi = getStoryblokApi();
   const { data } = await storyblokApi.getStory(`home`, {
-    version: process.env.NODE_ENV === "development" || isEnabled ? "draft" : "published",
-    resolve_relations: 'recommended_tours.tours',
-  })
-  return data.story
-}
+    version:
+      process.env.NODE_ENV === "development" || isEnabled
+        ? "draft"
+        : "published",
+    resolve_relations: "recommended_tours.tours",
+  });
+  return data.story;
+};
 
 export default async function Home() {
   const story = await fetchHomePage();
-  return <StoryblokStory story={story} />;
+  return (
+    <StoryblokStory
+      bridgeOptions={{ resolve_relations: ["recommended_tours.tours"] }}
+      story={story}
+    />
+  );
 }
